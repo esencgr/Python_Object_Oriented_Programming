06e25ff6168cb4ea75cf6fef1505fdb0546e5c7d
Cagri Esen <esencgr@gmail.com>
2021-03-03 23:29:53 +0300

    add

--- tv_remote.py
+++ tv_remote.py
@@ -7,44 +7,35 @@ class TV():
         self.channel_list = channel_list
         self.current_channel = current_channel
 
+    def info(self):
+        print(f"\nState of Television\n  sound = {self.sound}\n  channel list = {self.channel_list}\n  current channel = {self.current_channel}") 
 
-    def __str__(self):
-        return f"\nState of Television\n\tsound = {self.sound}\n\tchannel list = {self.channel_list}\n\tcurrent channel = {self.current_channel}"
+    # def __str__(self):
+    #     return f"\nState of Television\n  sound = {self.sound}\n  channel list = {self.channel_list}\n  current channel = {self.current_channel}"
 
     def set_sound(self):
+
+        print("Current sound level : ", self.sound)        
+
         while(True):
             res = input("\nPress - | + | S(save)\n") 
             
             if res == "-":
-                if self.sound > 10:
+                if self.sound >= 10:
                     self.sound -= 10
                     print("Sound level is : ", self.sound)
             
             elif res == "+":
-                if self.sound <= 100:
+                if self.sound < 100:
                     self.sound += 10
                     print("Sound level is : ", self.sound)
             
             else:
                 print("Sound is update : ", self.sound)
-                break
-
-    # def set_sound(self):
-    #     while True:
-    #         res = input("Ses - / Ses + / Quit q")
-    #         if (res == "-"):
-    #             if(self.sound != 0):
-    #                 self.sound -= 1
-    #                 print("Ses: ",self.sound)
-    #         elif (re == "+"):
-    #             if(self.sound != 10):
-    #                 self.sound += 1
-    #                 print("Ses: ",self.sound)
-    #         else:
-    #             break
- 
+                break 
 
     def add_channel(self):
+
         print("Current channel list : ", self.channel_list)
         new_ch = input("\nEnter new channel name : ")
         self.channel_list.append(new_ch)
@@ -52,21 +43,25 @@ class TV():
     
 
     def change_channel(self):
+
         while(True):
-            button = input("Please enter up '>'\ndown ''<'\nchannel number : ")
+            print("_"*20)
+            print("\nChannel list :", self.channel_list)
+            print("\nCurrent channel :", self.current_channel)
+            button = input("\n >  |  <  | channel number | S(save) :  ")
             current_indice = self.channel_list.index(self.current_channel)
-            total_channel = len(self.channel_list) - 1
+            total_channel = len(self.channel_list)
 
             if button == ">":
                 current_indice += 1
-                self.current_channel = self.channel_list[current_indice]
 
-                if current_indice > total_channel:
+                if current_indice > total_channel-1:
                     current_indice = 0
         
+                self.current_channel = self.channel_list[current_indice]
+
                 print("\nNow current channel is ",self.current_channel)
         
-
             elif button == "<":
                 current_indice -= 1
                 self.current_channel = self.channel_list[current_indice]
@@ -75,30 +70,36 @@ class TV():
                     current_indice = total_channel
 
                 print("\nNow current channel is ",self.current_channel)
-                 
+            
+            # elif button == "1":
+            #     pass
+
+            else:
+                print("Save current channel")
+                break 
 
 # ==========================================
+
 def main():
+    
     tv = TV()
+
     while(True):
         os.system("clear")
-
-
-        print(        #print menu
-        """
+        print("""
         MENU
         [1] -> STATE OF TV 
         [2] -> SOUND SETTING
         [3] -> ADD CHANNEL
         [4] -> CHANGE TO CHANNEL 
-        [Q] -> CLOSE TV
-        """)
+        [Q] -> CLOSE TV        """)
 
         case = input("Enter your process : ")
 
         # show current state
         if case == "1":
-            print(tv)
+            tv.info()
+            # print(tv)
             input("Press enter for menu.")
         
         # sound setting
